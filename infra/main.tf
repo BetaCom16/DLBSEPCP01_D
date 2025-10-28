246bd57e11601219f8d51a8871bf98ea0f875067
Erik Simmes <betacom16@outlook.de>
2025-10-28 08:49:39 +0100

    Temp. disabled CloudFront

--- infra/main.tf
+++ infra/main.tf
@@ -72,52 +72,52 @@ resource "aws_lambda_function_url" "app_lambda_url" {
   authorization_type = "NONE"
 }
 
-resource "aws_cloudfront_distribution" "app_cdn" {
-  origin {
-    domain_name = replace(aws_lambda_function_url.app_lambda_url.function_url, "https://", "")
-    origin_id   = "lambda-origin"
-
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "https-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
-    }
-  }
-
-  enabled             = true
-  is_ipv6_enabled     = true
-  
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "lambda-origin"
-    
-    viewer_protocol_policy = "redirect-to-https"
-
-    forwarded_values {
-      query_string = false
-      headers      = ["*"]
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 60
-    max_ttl                = 86400
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-}
+#resource "aws_cloudfront_distribution" "app_cdn" {
+#  origin {
+#    domain_name = replace(aws_lambda_function_url.app_lambda_url.function_url, "https://", "")
+#    origin_id   = "lambda-origin"
+#
+#    custom_origin_config {
+#      http_port              = 80
+#      https_port             = 443
+#      origin_protocol_policy = "https-only"
+#      origin_ssl_protocols   = ["TLSv1.2"]
+#    }
+#  }
+#
+#  enabled             = true
+#  is_ipv6_enabled     = true
+#  
+#  default_cache_behavior {
+#    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+#    cached_methods   = ["GET", "HEAD"]
+#    target_origin_id = "lambda-origin"
+#    
+#    viewer_protocol_policy = "redirect-to-https"
+#
+#    forwarded_values {
+#      query_string = false
+#      headers      = ["*"]
+#      cookies {
+#        forward = "none"
+#      }
+#    }
+#
+#    min_ttl                = 0
+#    default_ttl            = 60
+#    max_ttl                = 86400
+#  }
+#
+#  restrictions {
+#    geo_restriction {
+#      restriction_type = "none"
+#    }
+#  }
+#
+#  viewer_certificate {
+#    cloudfront_default_certificate = true
+#  }
+#}
 
 output "website_url" {
   description = "Die URL der Webseite"
